15ece720eb8b2db5374ef5770c66f6d4fb59f5af
tohka <seventeentohka@163.com>
2021-04-25 11:34:00 +0800

    4.25

---
+++ day4.22/src/com/offcn/demos/Demo1.java
@@ -0,0 +1,18 @@
+package com.offcn.demos;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        //1.2进制
+        int x2 = 0b10;
+        System.out.println(x2);
+        //2 8进制
+        int x8 = 010;
+        System.out.println(x8);
+        //3 10进制
+        int x10 = 15;
+        System.out.println(x10);
+        //4 16进制
+        int x16 = 0xa;
+        System.out.println(0xa);
+    }
+}
